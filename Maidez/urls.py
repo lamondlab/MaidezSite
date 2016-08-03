52ebdef1f044d8277562f3f4a88997ddea73fbd8
Rob Kent <rob@gulon.co.uk>
2016-08-03 16:44:37 +0100

    Adding RPC URL

--- Maidez/urls.py
+++ Maidez/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns=[
     url(r'^$', index, name="index"),
     url(r'^heartbeat/$', heartbeat, name="heartbeat"),
-    url(r'^rpc/$', rpc, name="rpc").
+    url(r'^rpc/$', rpc, name="rpc"),
 ]
